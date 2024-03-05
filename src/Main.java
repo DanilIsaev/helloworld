a24a0eef44fe2086dc7a573ebd84dffe428a3f45
Basilio <daniel.isaes@mail.ru>
2024-03-05 20:01:37 +0300

    Added variables: dog, cat, paper and output them

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,16 @@ public class Main {
         System.out.println("Привет, Мир!");
         System.out.println("Hello World!!!");
 
+        // Тема занятия переменные
+
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+
     }
 }
