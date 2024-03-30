e5df6a8f8da41bcdcf4b61c9309b8f9f65ab6068
Basilio <daniel.isaes@mail.ru>
2024-03-30 13:44:49 +0300

    add task 4 - cycle

--- src/Main.java
+++ src/Main.java
@@ -403,5 +403,11 @@ public class Main {
             System.out.println(i);
         }
 
+        // Задача 4
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+
     }
 }
